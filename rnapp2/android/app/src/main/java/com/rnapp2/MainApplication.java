094e06025f3581de7e4c4c8494bbfc7a493e71ba
unknown <you@example.com>
2019-12-16 00:22:11 -0500

    upgraded to react-native 0.61.5

--- rnapp2/android/app/src/main/java/com/rnapp2/MainApplication.java
+++ rnapp2/android/app/src/main/java/com/rnapp2/MainApplication.java
@@ -3,11 +3,12 @@ package com.rnapp2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import java.util.Arrays;
 import java.util.List;
---
+++ rnapp2/react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    assets: ['./assets/fonts/'],
+    dependencies: {
+        "react-native-gesture-handler": {
+          platforms: {
+            android: null,
+            ios: null
+          }
+        }
+      }
+    
+  };
